c610b988e061734cd92fdad81f650b036f59fa2f
Azizjonson <xabibullayevamohira87@gmail.com>
2024-08-28 16:48:44 +0500

    js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var name = prompt("Ismingizni Kiriting");
+
+var age = prompt("Yoshingizni Kiriting");
+
+var math1 = prompt("Masalani Yeching: 9 + 8 = ?");
+
+var math2 = prompt("2-Masalani Yeching: 25 - 7 = ?");
+
+var math3 = prompt("3-Masalani Yeching: 20 * 3 = ?");
+
+var math4 = prompt("4-Masalani Yeching: 8/2");
+
+var math5 = prompt("5-Masalani Yeching: 20 % 3 = ?");
+
+alert("Consolni oching!")
+
+console.log("Sizning Ismingiz "+name);
+
+console.log("Sizning Yoshingiz "+age);
+
+console.log("Masala 9 + 8 = 17  Sizning Javobingiz: "+math1);
+
+console.log("2-Masala 25 - 7 = 18  Sizning Javobingiz: "+math2);
+
+console.log("3-Masala 20 * 3 = 60  Sizning Javobingiz: "+math3);
+
+console.log("4-Masala 8 / 2 = 4  Sizning Javobingiz: "+math4);
+
+console.log("5-Masala 20 % 3 = 2  Sizning Javobingiz: "+math5);
